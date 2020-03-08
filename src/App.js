b8f3de7d2289b7afca4cae9697505d47ca270833
halfmin <halfmin@gmail.com>
2020-03-08 23:45:20 +0900

    init

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
+<div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -21,6 +21,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
 export default App;
